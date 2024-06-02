0ee7bedf78c9edcbb6de5b30ef5fa2d0fcbd9ccc
dinu <patildinesh7742@gmail.com>
2024-06-02 19:07:09 +0530

    react-evaluation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,24 @@ import axios from "axios"
 import {Link} from 'react-router-dom'
 import LoadingIndicator from "../components/LoadingIndicator"
 import ErrorIndicator from "../components/ErrorIndicator"
+import {
+   Button,
+   Container,
+   SimpleGrid,
+   Box,
+   Flex,
+   Heading,
+   Select,
+   HStack,
+ } from "@chakra-ui/react";
 
 export default function Home(){
     const [data,setData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
+    const [sortOrderValue, setSortOrderValue] = useState("");
+    const [filterValue, setFilterValue] = useState("");  
+    
      
      async function fetchDataUpdate(){
         setLoading(true)
@@ -39,16 +52,50 @@ export default function Home(){
      }
 
     return(
-    <><h1>Home page</h1>
-       
-           {data.map(product=>(
-            <div key={product.id} style={{border:'2px solid'}}>
-               <h2>Title : {product.title}</h2>
-                <h3>Category : {product.category}</h3> 
-                <h3>Price:{product.price}</h3>
-                <Link to='/Product-Details'>More Details...</Link>
-            </div>
-           ))} 
+    <>
+    <Box>
+      <Heading align="center">HOME PAGE</Heading>
+    </Box>
+    <Container maxW="container.xl">
+        <Flex justify="end">
+        <HStack spacing={4} my={4}>
+            <Select
+               placeholder="Sort by Price"
+               value={sortOrderValue}
+               onChange={(e) => {
+                  setSortOrderValue(e.target.value);
+               }}
+            >
+               <option value="asc">Low to High</option>
+               <option value="desc">High to Low</option>
+            </Select>
+            <Select
+               placeholder="Filter by Category"
+               value={filterValue}
+               onChange={(e) => setFilterValue(e.target.value)}
+            >
+               <option value="men">Men</option>
+               <option value="women">Women</option>
+               <option value="kids">Kids</option>
+               <option value="home decor">Home Decor</option>
+            </Select>
+         </HStack>
+        </Flex>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
+         {data.map(product=>(
+               <div key={product.id} style={{border:'2px solid', height:"130px"}}>
+                  <h2>Title : {product.title}</h2>
+                  <h3>Category : {product.category}</h3> 
+                  <h3>Price:{product.price}</h3>
+                  <Link to='/Product-Details'>
+                     <Button variant="outline" colorScheme="red"> More Details...</Button>
+                     </Link>
+               </div>
+            ))} 
+         </SimpleGrid>
+      </Container>
+           
     </>
     )
 }
